143685541392c73e99a0cf5c85bd587d60b35a88
Shabero <tokmokiuca@gmail.com>
2023-10-27 00:29:31 +0600

    nwee

---
+++ script.js
@@ -0,0 +1,66 @@
+// let a = "Hello"
+// let b = "Ivan"
+// console.log(a + b);
+
+// записать длину в переменную b
+// let a =  'lorem ipsum dolor'
+// let b = a.length;
+// console.log(b);
+
+// вернуть sit lorem ipsum dolor amet
+// let a = 'lorem ipsum dolor sit amet';
+// let result = a.split(' ').slice(3).concat(a.split(' ').slice(0, 3)).join(' ');
+// console.log(result); 
+
+//вернуть CHILDREn
+// let a = 'children'
+// let result = a.toUpperCase().slice(0,7)+a.slice(7).toLowerCase();
+// console.log(result);
+
+// вернуть bsso,
+// let a = 'boss'
+// let result = a[0] + a[2] + a[3] + a[1];
+// console.log(result);
+
+// вернуть index d 
+// let a = 'sunday'
+// let result = a.indexOf('d');
+// console.log(result);
+
+// // вернуть hekso
+// let a = 'hello'
+// let replaceA = 2;
+// let replaceB = 3;
+// let replaceC = 'k';
+// let replaceD = 's';
+// console.log(b);
+
+// вернуть new us
+// let a = 'user'
+// let b = 'new'
+// let c = a.slice(0, 2);
+// console.log(b + ' ' + c);
+
+
+// вернуть daydaymon
+// let a = 'monday'
+// let b = 'day'
+// let c = b.slice()+b.slice()+a.slice(0,3);
+// console.log(c);
+
+// // вернуть 'hElLo'
+// let a = 'HELLO'
+// let b = a[0,3].toLowerCase();
+// console.log(b);
+
+// вернуть '450'
+// let a = 5;
+// let b = '400';
+// let result = (a + parseInt(b)).toString(); 
+// console.log(result);
+
+
+// вернуть manak
+let a = 'monkey'
+let b = 'banana'
+
